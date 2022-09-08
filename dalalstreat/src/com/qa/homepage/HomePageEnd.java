6216d02d249db026f18e86384f2ec6a8233ddeb5
santanu maity <santanumaityisback@gmail.com>
2022-09-08 06:52:44 +0530

    HomePage Feature Commit
    I have complete HomePage Feature plese ckeck and review so tha t i can merge it.

---
+++ dalalstreat/src/com/qa/homepage/HomePageEnd.java
@@ -0,0 +1,9 @@
+package com.qa.homepage;
+
+public class HomePageEnd {
+	public static void main(String args[])
+	{
+		System.out.println("Homepage End");
+	}
+
+}
---
+++ dalalstreat/src/com/qa/homepage/HomePageStart.java
@@ -0,0 +1,9 @@
+package com.qa.homepage;
+
+public class HomePageStart {
+	public static void main(String args[])
+	{
+	System.out.println("homePage start");
+
+	}
+}
